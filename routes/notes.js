4349946d1587f5224c23fcf4f0979e3b61159228
VShreeKrishna <vshreekrishna2110@gmail.com>
2024-03-16 02:01:55 +0530

    modified changes

--- routes/notes.js
+++ routes/notes.js
@@ -46,6 +46,8 @@ router.post('/addnote',fetchuser,[
  // ROUTE:3: updatenotes: PUT "/api/notes/updatenote".login required
 
 router.put('/updatenote/:id',fetchuser,async (req,res)=>{
+  try {
+   
    const{title,description,tag}=req.body;
    const newNote ={};
    if(title) {newNote.title=title};
@@ -63,6 +65,38 @@ router.put('/updatenote/:id',fetchuser,async (req,res)=>{
   //to set the updated note
   note =await Note.findByIdAndUpdate(req.params.id, {$set:newNote},{new:true})
   res.json({note})
+} 
+catch (error) {
+  console.error("Error saving user:", error)
+  res.status(500).json({ message: "Server error" })
+}
+
+})
+
+
+
+// ROUTE:4: deleting an existing note :updatenotes: DELETE "/api/notes/deletenote".login required
+
+router.delete('/deletenote/:id',fetchuser,async (req,res)=>{
+  try {
+    
+    //find the note to be delete and delete it
+let note = await Note.findById(req.params.id);
+ if( !note ) {res.status(404).send("Not Found")}
+
+ //allow deletion if the owner owns the note
+ if(note.user.toString()!==req.user.id){
+   return res.status(401).send("Not allowed")
+ }
+ 
+ //to set the updated note
+ note =await Note.findByIdAndDelete(req.params.id)
+ res.json({"success":"Note has been deleted",note:note})
+}
+ catch (error) {
+  console.error("Error saving user:", error)
+  res.status(500).json({ message: "Server error" })
+}
 })
 
 module.exports =router
